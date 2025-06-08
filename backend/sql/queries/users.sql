7901dd30eb79df5c90a7d52366ddde61f3507893
Yousef Amirghofran <yamirghofran@gmail.com>
2025-06-08 18:35:58 +0200

    users: write CRUD SQL queries for users

---
+++ backend/sql/queries/users.sql
@@ -0,0 +1,21 @@
+-- name: GetUsers :many
+SELECT * FROM users;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: CreateUser :one
+INSERT INTO users (name, email, password_hash)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: UpdateUser :one
+UPDATE users
+SET name = $2, email = $3, password_hash = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :one
+DELETE FROM users
+WHERE id = $1;
